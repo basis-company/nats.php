452786c49c68dad009422eb9e4200ae585a97ee7
Christoffer Lindahl <christoffer@trinax.se>
2023-12-08 15:53:25 +0100

    feat: Fetching all stored key value pairs in bucket.

--- src/KeyValue/Bucket.php
+++ src/KeyValue/Bucket.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Basis\Nats\KeyValue;
 
 use Basis\Nats\Client;
+use Basis\Nats\Consumer\Configuration as ConsumerConfiguration;
 use Basis\Nats\Stream\Stream;
 use Basis\Nats\Message\Payload;
 
@@ -45,6 +46,39 @@ class Bucket
         $value = base64_decode($response->message->data);
 
         return new Entry($this->name, $key, $value, $revision);
+    }
+
+    /**
+     * @return Entry[]
+     */
+    public function getAll(): array
+    {
+        $entries = [];
+
+        $stream = $this->getStream();
+        if (!$stream->exists()) {
+            return $entries;
+        }
+
+        $stream_name = $stream->getName();
+        $configuration = new ConsumerConfiguration($stream_name);
+        $consumer = $stream->createEphemeralConsumer($configuration);
+        $subject_prefix_length = 1 + strlen(sprintf('$KV.%s', $this->name));
+
+        $consumer->handle(function (Payload $payload) use (&$entries, $subject_prefix_length) {
+            if ($payload->subject === null) {
+                return;
+            }
+
+            $key = substr($payload->subject, $subject_prefix_length);
+            $entries[] = new Entry('', $key, $payload->body, 0);
+        }, function () use ($consumer) {
+            $consumer->interrupt();
+        });
+
+        $consumer->delete();
+
+        return $entries;
     }
 
     public function getStatus(): Status
