c06eef3aa1354fba43e44eba9413acf5e5bac433
dmitry krokhin <nekufa@gmail.com>
2024-12-25 17:30:38 +0300

    extend message tests

--- tests/Unit/Message/FactoryTest.php
+++ tests/Unit/Message/FactoryTest.php
@@ -13,20 +13,34 @@ class FactoryTest extends TestCase
     public function testInfo()
     {
         $infoData = [
-            "server_id" => "ID",
-            "server_name" => "NAME",
-            "version" => "1.0",
+            "headers" => false,
+            "port" => 1234,
             "proto" => 1,
             "go" => "1",
             "host" => "host",
-            "port" => 1234,
-            "headers" => false,
+            "server_id" => "ID",
+            "server_name" => "NAME",
+            "version" => "1.0",
             "domain" => "domain"
         ];
         $infoString = "INFO " . json_encode($infoData);
 
         $message = Factory::create($infoString);
 
-        $this->assertEquals(Info::class, get_class($message));
+        $this->assertInstanceOf(Info::class, $message);
+        $this->assertEquals(get_object_vars($message), $infoData);
+        $this->assertSame($infoString, $message->render());
+    }
+
+    public function testRendering()
+    {
+        $this->assertSame("PONG", Factory::create("PONG")->render());
+        $this->assertSame("+OK", Factory::create("+OK")->render());
+    }
+
+    public function testInvalidMessage()
+    {
+        $this->expectExceptionMessage("Parse message failure: TEST");
+        Factory::create("TEST");
     }
 }
