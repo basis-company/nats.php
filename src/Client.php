9552b46e14ce297459378cf6cc8c494e3cdf6c1a
Dimitar Ilkov <chazzbg@gmail.com>
2026-02-02 18:03:59 +0200

    cs fix

--- src/Client.php
+++ src/Client.php
@@ -189,7 +189,7 @@ class Client
                 return $result;
             }
             if (array_key_exists($message->sid, $this->handlers)) {
-                if(isset($message->payload->headers['Status-Code']) && $message->payload->headers['Status-Code'] === '404') {
+                if (isset($message->payload->headers['Status-Code']) && $message->payload->headers['Status-Code'] === '404') {
                     return null;
                 }
                 return $this->processMsg($this->handlers[$message->sid], $message, $reply);
